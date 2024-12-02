9c950f846dae238cd00c41a0ceddd09d4f03f739
bngams <n.boris@devidia.net>
2024-12-02 13:14:38 +0000

    Config projet, main, args

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println("My args are:");
+        // parcours du tableau
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
